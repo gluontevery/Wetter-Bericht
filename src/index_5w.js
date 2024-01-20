30c37e6c2d39912b4db77df53b79cad412cf74f0
gluontevery <gluontevery@users.noreply.github.com>
2024-01-20 23:38:30 +0100

    add forecast items

--- src/index_5w.js
+++ src/index_5w.js
@@ -1,14 +1,18 @@
+function geoCoordFromCityName(city){
+  let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+  axios.get(apiUrl).then(apiDataGeo);
+}
+
 function onPressButton(event) {
   event.preventDefault();
   let textField = document.querySelector("form .search-input");
   let city = textField.value;
   city = city.trim().toLowerCase();
   city = capitalise(city);
-  let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
-  axios.get(apiUrl).then(apiDataGeo);
+  geoCoordFromCityName(city);
   htmlUpdateCity(city);
   textField.value = null;
-  htmlUpdateDate();
+  htmlUpdateDate(city);
 }
 
 function capitalise(cityName) {
@@ -25,7 +29,61 @@ function htmlUpdateCity(cityName) {
   h1City.innerHTML = cityName;
 }
 
-function htmlUpdateDate() {
+function forecastGenerator(cityName, days, day) {
+  let dayForecast="";
+  let forecastHTML="";
+  const forecastSection = document.querySelector(".forecast");
+
+  for (let i = 1; i < 7; i++) {
+    day = day + 1;
+    if (day>6) {
+      day=day-7;
+    }
+    dayForecast=days[day].substring(0,3);
+    forecastHTML=forecastHTML+`<div class="day-forecast">
+                <div class="day-forecast-name">${dayForecast}</div>
+                <div class="icon-wrapper" id="icon${i}">
+                  <img src="src/images/icons/sun100.png" class="day-forecast-icon" alt="🌤">
+                </div>
+                <div class="day-forecast-temperature" id="tempDay${i}">
+                  <span class="day-forecast-temp">15°</span>
+                  <span class="day-forecast-temp-realFeel">9°</span>
+                </div>
+              </div>
+                `;
+  }
+  forecastSection.innerHTML=forecastHTML;
+  geoCoordFromCityName(cityName);
+}
+
+function forecastInserter(response) {
+ console.log(response);
+ let idTemp="";
+ let idIcon="";
+ let icon="";
+  for (let i = 1; i < 7; i++) {
+    idTemp=`#tempDay${i}`;
+    let tempSection = document.querySelector(idTemp);
+    tempSection.innerHTML=`<span class="day-forecast-temp">
+                                ${Math.round(response.data.daily[i].temp.eve)}°
+                           <span class="tooltip">Average temp</span>
+                           </span>
+                           <span class="day-forecast-temp-realFeel">
+                                ${Math.round(response.data.daily[i].feels_like.eve)}°
+                           <span class="tooltip">Real-feel temp</span>
+                           </span>`;
+    //console.log(`temperatura in ${i}day ${response.data.daily[i].temp.eve}`);
+    //console.log(`RealFeel temp in ${i}day ${response.data.daily[i].feels_like.eve}`)
+    idIcon=`#icon${i}`;
+    let iconSection = document.querySelector(idIcon);
+    icon = dayWeatherIconSelection(response.data.daily[i].clouds);
+    iconSection.innerHTML=`<img src="${icon}" class="day-forecast-icon" alt="weather icon">`;
+    //console.log(`Clouds density in ${i}day ${response.data.daily[i].clouds}`)
+  }
+}
+
+
+function htmlUpdateDate(cityName) {
   let days = [
     "Sunday",
     "Monday",
@@ -71,6 +129,12 @@ function htmlUpdateDate() {
   }
 
   currentDayTime.innerHTML = `${days[day]} ${hours}:${minutes}`;
+  forecastGenerator(cityName, days, day)
+}
+
+function forecastApiRequest(lat, lon) {
+  const forecastUrlApi=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&appid=${apiKey}&units=metric`;
+  axios.get(forecastUrlApi).then(forecastInserter);
 }
 
 function apiDataGeo(response) {
@@ -81,6 +145,7 @@ function apiDataGeo(response) {
     let lat = response.data[0].lat;
     let lon = response.data[0].lon;
     let apiUrlGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    forecastApiRequest(lat, lon)
     axios.get(apiUrlGeo).then(changeTemperature);
   } catch (err) {
     //console.log(`The error is: ${err.message}`);
@@ -98,7 +163,7 @@ function apiDataCity(response) {
     temperature: 0,
     humidity: 0,
     wind: 0,
-    cloudy: 0,
+    cloudy: 0,  
     cityTime: 0,
     sunRise: 0,
     sunSet: 0,
@@ -112,7 +177,34 @@ function apiDataCity(response) {
   weatherData.sunRise = response.data.sys.sunrise;
   weatherData.sunSet = response.data.sys.sunset;
   weatherData.description = response.data.weather[0].description;
+
   return weatherData;
+}
+
+function dayWeatherIconSelection(cloudy) {
+  let icon="";
+  if (cloudy>25 && cloudy<51) {
+    icon = "src/images/icons/sun50.png";
+  } else if (cloudy>=51 && cloudy<76) {
+    icon = "src/images/icons/sun25.png";
+  } else if (cloudy>=76 && cloudy<85) {
+    icon = "src/images/icons/clouds.png";
+  } else if (cloudy>=85) {
+    icon = "src/images/icons/clouds_thick.png";
+  } else {
+    icon = "src/images/icons/sun100.png";
+  }
+  return icon;
+}
+
+function nightWeatherIconSelection(cloudy) {
+  let icon="";
+  if (cloudy<30) {
+    icon = "src/images/icons/night_clear_sky.png";
+  } else if (cloudy>=30 && cloudy<76) {
+    icon = "src/images/icons/night_sky_clouds50.png";
+  }
+  return icon;
 }
 
 function changeTemperature(response) {
@@ -135,22 +227,12 @@ function changeTemperature(response) {
 
   const weatherIcon= document.querySelector(".current-temperature-icon");
   if (weatherData.cityTime>weatherData.sunSet || weatherData.cityTime<weatherData.sunRise) {
-    if (weatherData.cloudy<30) {
-      weatherIcon.src = "src/images/icons/night_clear_sky.png";
-    } else if (weatherData.cloudy>=30 && weatherData.cloudy<76) {
-      weatherIcon.src = "src/images/icons/night_sky_clouds50.png";
-    }
+    weatherIcon.src = nightWeatherIconSelection(weatherData.cloudy);
   } else {
-    if (weatherData.cloudy>25 && weatherData.cloudy<51) {
-      weatherIcon.src = "src/images/icons/sun50.png";
-    } else if (weatherData.cloudy>=51 && weatherData.cloudy<76) {
-      weatherIcon.src = "src/images/icons/sun25.png";
-    } else if (weatherData.cloudy>=76 && weatherData.cloudy<85) {
-      weatherIcon.src = "src/images/icons/clouds.png";
-    }
+    weatherIcon.src = dayWeatherIconSelection(weatherData.cloudy);
   }
   if (weatherData.cloudy>=85) {
-    weatherIcon.src = "src/images/icons/clouds_thick.png";
+    weatherIcon.src = dayWeatherIconSelection(weatherData.cloudy);
   }
 }
 
@@ -158,11 +240,7 @@ const apiKey = "5201594abea9f3e38b70e65b11a80c24";
 const units = "metric";
 let reloadsCount=0;
 
-window.onload = htmlUpdateDate();
-window.onload = (()=> {
-  let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=Paris&limit=5&appid=${apiKey}`;
-  axios.get(apiUrl).then(apiDataGeo);
-});
+window.onload = htmlUpdateDate("Paris");
 
 let citySearchForm = document.querySelector("#search-form");
 citySearchForm.addEventListener("submit", onPressButton);
